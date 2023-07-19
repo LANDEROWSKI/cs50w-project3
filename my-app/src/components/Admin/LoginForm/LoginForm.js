bfe37cfe443f99c5d79749089cd66854f9e8a42e
LANDEROWSKI <110938474+LANDEROWSKI@users.noreply.github.com>
2023-07-19 16:17:48 -0600

    hacemos las validaciones de los campos del formulario

--- my-app/src/components/Admin/LoginForm/LoginForm.js
+++ my-app/src/components/Admin/LoginForm/LoginForm.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import {Button, Form} from 'semantic-ui-react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 import './LoginForm.scss'
 
 export  function LoginForm() {
+  const formik = useFormik({
+      initialValues: initialValues(),
+      validationSchema: Yup.object(validationSchema()),
+      onSubmit:(formValue)=>{
+        console.log('Email enviado')
+        console.log(formValue);
+      }
+  })
+
+
   return (
-    <Form className='login-form-admin'>
+    <Form className='login-form-admin' onSubmit={formik.handleSubmit}>
         <Form.Input
             name="email"
             placeholder="Correo Electronico"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            error={formik.errors.email}
         />
         <Form.Input
             name="password"
             type='password'
             placeholder="Contraseña"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            error={formik.errors.password}            
         />
         <Button 
             type='submit'
@@ -21,4 +39,18 @@ export  function LoginForm() {
         />
     </Form>
   )
+}
+
+function initialValues(){
+  return{
+    email: "",
+    password: "",
+  }
+}
+
+function validationSchema(){
+  return{
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true), 
+  }
 }
